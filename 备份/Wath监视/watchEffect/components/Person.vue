3e62c063ff1df86c526312ffb5bd7a847dbcdef3
林金滨 <2059637505@qq.com>
2024-08-11 14:34:19 +0800

    vue3

---
+++ 备份/Wath监视/watchEffect/components/Person.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="Person"> 
+       <h2>水位的监视</h2>
+       <h2>水位:{{ temp }}</h2>
+       <h2>温度:{{ height }}</h2>
+       <button @click = "changeTemp">水温加10</button>
+       <button @click = "changeHeight">水位加10</button>
+    </div>
+</template>
+<script>
+export default 
+{
+    name:'Person'//组件名  x
+}
+</script >
+<script  setup >
+import { ref ,watch,reactive,watchEffect} from 'vue'
+//数据
+
+let temp = ref(0)
+let height = ref(0)
+
+//方法
+function changeTemp()
+{
+   temp.value += 10 
+
+}
+
+function changeHeight()
+{
+  height.value +=10
+}
+
+
+//监测
+
+watchEffect(()=>{
+if(temp.value>=60 ||height.value>=80)
+console.log("向服务器发送消息")
+})
+
+</script>
+<style scoped>
+.Person
+{
+    background-color:rgb(141, 2, 247) ;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding:10px;
+
+}
+button
+{
+    margin: 0 5px ;
+}
+</style>
---
+++ 备份/标签ref/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="Person"> 
+      <h1>中国</h1>
+      <h2 ref = "title1">福建</h2>
+      <h2>小明</h2>
+      <button @click="showLog">点我输出这个元素</button>
+    </div>
+</template>
+<script>
+export default 
+{
+    name:'Person'//组件名  x
+}
+</script >
+<script  setup  name = "Person">
+import { ref,watch,reactive,watchEffect,defineExpose} from 'vue'
+//数据
+let title1= ref()//用于存储ref标记的内容
+let a = ref(0)
+let b = ref(1)
+let c = ref(2)
+
+//方法
+function showLog()
+{
+    console.log(title1.value);
+    
+}
+
+defineExpose({a,b,c})
+
+
+//监测
+
+</script>
+<style scoped>
+.Person
+{
+    background-color:rgb(141, 2, 247) ;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding:10px;
+
+}
+button
+{
+    margin: 0 5px ;
+}
+</style>
